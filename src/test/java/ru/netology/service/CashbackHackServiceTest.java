32e6ef016f1fe2069f9d4ac3a93b30c39cfebfaf
arsh174 <74has@mail.ru>
2024-04-07 13:55:12 +0500

    reformateCode

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -7,10 +7,10 @@ import static org.junit.Assert.*;
 public class CashbackHackServiceTest {
 
     @Test
-    public void testRemain(){
+    public void testRemain() {
         CashbackHackService service = new CashbackHackService();
 
-        int actual =  service.remain(2000);
+        int actual = service.remain(2000);
         int expected = 1000;
         assertEquals(expected, actual);
     }
